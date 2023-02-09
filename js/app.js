c3091392c51b541d7d1d216aac938b51b7b15430
ReeceRenninger <renningerreece@gmail.com>
2023-02-09 14:12:31 -0800

    added function two

--- js/app.js
+++ js/app.js
@@ -4,57 +4,58 @@
 
 // To Do: Get user name - prompt and alert greeting back to user
 
-let userName = prompt("What is your name?");
+let userName = prompt('What is your name?');
 let userScore = 0;
 
 alert(`Welcome to my site, ${userName}! Let's play a guessing game. Please answer "yes" or "no", or answer "y" or "n"`);
 
 function questionOne (){
 // To Do: Prompt 5 yes/no questions for the user to guess and alert when they are correct/incorrect
-  let questionOneGuess = prompt("Do I have a daughter?").toLowerCase();
+  let questionOneGuess = prompt('Do I have a daughter?').toLowerCase();
   if (questionOneGuess === 'yes' || questionOneGuess === 'y') {
-    alert("You Are Correct!");
+    alert('You Are Correct!');
     userScore += 1;
   } else if (questionOneGuess === 'no' || questionOneGuess === 'n') {
-    alert("You Are Incorrect");
+    alert('You Are Incorrect');
   }
 }
 questionOne();
 
-let questionTwoGuess = prompt("Was I in the Army?").toLowerCase();
-
-if (questionTwoGuess === 'no' || questionTwoGuess === 'n') {
-    alert("You Are Correct!");
+function questionTwo(){
+  let questionTwoGuess = prompt('Was I in the Army?').toLowerCase();
+  if (questionTwoGuess === 'no' || questionTwoGuess === 'n') {
+    alert('You Are Correct!');
     userScore += 1;
-} else if (questionTwoGuess === 'yes' || questionTwoGuess === 'y') {
-    alert("You Are Incorrect");
+  } else if (questionTwoGuess === 'yes' || questionTwoGuess === 'y') {
+    alert('You Are Incorrect');
+  }
 }
+questionTwo();
 
-let questionThreeGuess = prompt("Am I a student at CodeFellows?").toLowerCase();
-
+let questionThreeGuess = prompt('Am I a student at CodeFellows?').toLowerCase();
 if (questionThreeGuess === 'yes' || questionThreeGuess === 'y') {
-    alert("You Are Correct!");
+    alert('You Are Correct!');
     userScore += 1;
 } else if (questionThreeGuess === 'no' || questionThreeGuess === 'n') {
-    alert("You Are Incorrect");
+    alert('You Are Incorrect');
 }
 
-let questionFourGuess = prompt("Do I live in Oregon?").toLowerCase();
+let questionFourGuess = prompt('Do I live in Oregon?').toLowerCase();
 
 if (questionFourGuess === 'no' || questionTwoGuess === 'n') {
-    alert("You Are Correct!");
+    alert('You Are Correct!');
     userScore += 1;
 } else if (questionTwoGuess === 'yes' || questionFourGuess === 'y') {
-    alert("You Are Incorrect");
+    alert('You Are Incorrect');
 }
 
-let questionFiveGuess = prompt("Am I married?").toLowerCase();
+let questionFiveGuess = prompt('Am I married?').toLowerCase();
 
 if (questionFiveGuess === 'yes' || questionFiveGuess === 'y') {
-    alert("You Are Correct!");
+    alert('You Are Correct!');
     userScore += 1;
 } else if (questionFiveGuess === 'no' || questionFiveGuess === 'n') {
-    alert("You Are Incorrect");
+    alert('You Are Incorrect');
 }
 
 // Lab03 Objectives
